beed92626f4e930093feeb7a7976cff64a3140f5
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-06-26 17:10:01 -0400

    format

--- varius/display.py
+++ varius/display.py
@@ -22,22 +22,17 @@ def bracket(expr: Any, *ops: str) -> str:
 
 
 def sadd(v1: Any, v2: Any):
-
     try:
         if isinstance(v1, str) or isinstance(v2, str):
             raise TypeError
         return repr(v1 + v2)
     except TypeError:
-
         if v1 == 0:
             return str(v2)
-
         if v2 == 0:
             return str(v1)
-
         if v1 == v2:
             return smul(2, v1)
-
         v1 = bracket(v1, Ops.NEG, Ops.POS)
         v2 = bracket(v2, Ops.NEG, Ops.POS)
         return f"{v1} {Ops.ADD} {v2}"
@@ -49,35 +44,26 @@ def sneg(v: Any):
             raise TypeError
         return repr(-v)
     except TypeError:
-
         if v == 0:
             return "0"
-
         if isinstance(v, str) and v.startswith("-"):
             return v[1:]
-
         v = bracket(v, Ops.NEG, Ops.POS)
-
         return f"{Ops.NEG}{v}"
 
 
 def ssub(v1: Any, v2: Any):
-
     try:
         if isinstance(v1, str) or isinstance(v2, str):
             raise TypeError
         return repr(v1 - v2)
     except TypeError:
-
         if v1 == 0:
             return sneg(v2)
-
         if v2 == 0:
             return str(v1)
-
         if v1 == v2:
             return "0"
-
         v1 = bracket(v1, Ops.NEG, Ops.POS)
         v2 = bracket(v2, Ops.NEG, Ops.POS)
         return f"{v1} {Ops.SUB} {v2}"
@@ -87,25 +73,18 @@ def smul(v1: Any, v2: Any):
     try:
         if isinstance(v1, str) or isinstance(v2, str):
             raise TypeError
-
         return repr(v1 * v2)
     except TypeError:
-
         if v1 == 1.0:
             return str(v2)
-
         if v2 == 1.0:
             return str(v1)
-
         if v1 == 0 or v2 == 0:
             return "0"
-
         if v1 == v2:
             return spow(v1, 2)
-
         v1 = bracket(v1, Ops.NEG, Ops.POS, Ops.MOD)
         v2 = bracket(v2, Ops.NEG, Ops.POS, Ops.MOD)
-
         return f"{v1} {Ops.MUL} {v2}"
 
 
@@ -113,47 +92,35 @@ def sdiv(v1: Any, v2: Any):
     try:
         if isinstance(v1, str) or isinstance(v2, str):
             raise TypeError
-
         return repr(v1 / v2)
     except TypeError:
         if v2 == 0:
             raise ZeroDivisionError
-
         if v2 == 1:
             return str(v1)
         if v1 == 0:
             return "0"
-
         if v1 == v2:
             return "1"
-
-        else:
-            v1 = bracket(v1, Ops.NEG, Ops.POS)
-            v2 = bracket(v2, Ops.NEG, Ops.POS)
-            return f"{v1} {Ops.DIV} {v2}"
+        v1 = bracket(v1, Ops.NEG, Ops.POS)
+        v2 = bracket(v2, Ops.NEG, Ops.POS)
+        return f"{v1} {Ops.DIV} {v2}"
 
 
 def spow(base: Any, exponent: Any):
     try:
         if isinstance(base, str) or isinstance(base, str):
             raise TypeError
-
         return repr(base ** exponent)
     except TypeError:
-
         if base == 0:
             raise ValueError("Power base cannot be zero.")
-
         if base == 1:
             return "1"
-
         if exponent == 0:
             return "1"
-
         if exponent == 1:
             return str(base)
-
         base = bracket(base, Ops.ADD, Ops.SUB, Ops.MUL, Ops.DIV, Ops.MOD)
         exponent = bracket(exponent, Ops.ADD, Ops.SUB, Ops.MUL, Ops.DIV, Ops.MOD)
-
         return f"{base}{Ops.POW}{exponent}"
