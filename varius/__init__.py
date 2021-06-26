5513cafb5755835f0b8a071c787a9d7fac5b7e35
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-06-26 14:20:02 -0400

    fix init order

--- varius/__init__.py
+++ varius/__init__.py
@@ -1,9 +1,9 @@
 from typing import Dict, Optional
 
-from .variable import *
-
 VARIABLE_STORAGE: Dict[str, Dict[str, float]] = {}
 
 _CURRENT_VERSION: Optional[str] = None
+
+from .variable import *
 
 __version__ = "0.1.0"
